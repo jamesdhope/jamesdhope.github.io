d74f6c67d9fa6e7be0d8c1bc53fd4585c96d79bb
James Hope <james.hope@ibm.com>
2023-10-31 20:32:21 +0000

    updated

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,79 +1,79 @@
 var store = [{
         "title": "Deep Learning Recurrent Neural Net for Time Series Forecasting",
-        "excerpt":"The Notebook described here supersedes my basic Recurrent Neural Net (RNN) for predicting multiple time series for the Wikipedia page forecasting competition. Instead, this RNN is constructed using the GRUCell rather than the BasicRNNCell which provides the model with persistence in the patterns observed over the series. The introductory paper...","categories": ["kaggle"],
-        "tags": ["data science","machine learning"],
-        "url": "/kaggle/time-series-forecasting/",
+        "excerpt":"The Notebook described here supersedes my basic Recurrent Neural Net (RNN) for predicting multiple time series for the Wikipedia page forecasting competition. Instead, this RNN is constructed using the GRUCell rather than the BasicRNNCell which provides the model with persistence in the patterns observed over the series. The introductory paper...","categories": ["data science"],
+        "tags": ["data science","machine learning","neural networks","time series prediction","tensorflow","pandas","predictive modelling"],
+        "url": "/data%20science/time-series-forecasting/",
         "teaser": null
       },{
         "title": "Zillow's Zestimate, and my ensemble of regressors for highly featured data prediction",
-        "excerpt":"“The Zillow Prize contest competition, sponsored by Zillow, Inc. (“Sponsor”) is open to all individuals over the age of 18 at the time of entry. The competition will contain two rounds, one public and one private.. Each round will have separate datasets, submission deadlines and instructions on how to participate....","categories": ["kaggle"],
-        "tags": [],
-        "url": "/kaggle/zillow-ensemble/",
+        "excerpt":"“The Zillow Prize contest competition, sponsored by Zillow, Inc. (“Sponsor”) is open to all individuals over the age of 18 at the time of entry. The competition will contain two rounds, one public and one private.. Each round will have separate datasets, submission deadlines and instructions on how to participate....","categories": ["data science"],
+        "tags": ["data science","machine learning","ensemble","regression","kaggle","python","gradient boosting","random forest","extra trees","feature engineering","predictive modelling"],
+        "url": "/data%20science/zillow-ensemble/",
         "teaser": null
       },{
         "title": "Implementation of the Stable Marriage Algorithm.",
-        "excerpt":"Ever been faced with the challenge of pairing people based on their individual preferences? I can think of numerous situations in which such a task might arise, for example, in pairing up co-workers for a task or pairing up students for an activity. If each student were to (1) list...","categories": ["edtech,","coding,","programming,","python,","computing,","computer-science"],
-        "tags": [],
-        "url": "/edtech,/coding,/programming,/python,/computing,/computer-science/stable-marriage/",
+        "excerpt":"Ever been faced with the challenge of pairing people based on their individual preferences? I can think of numerous situations in which such a task might arise, for example, in pairing up co-workers for a task or pairing up students for an activity. If each student were to (1) list...","categories": ["data science"],
+        "tags": ["data science","stable marriage","algorithmic programming","python"],
+        "url": "/data%20science/stable-marriage/",
         "teaser": null
       },{
         "title": "Implementation of the Stable Marriage Algorithm to find 'Stable Groups'",
-        "excerpt":"In my previous post, I explained my implementation of the Stable Marriage Algorithm to find stable pairs. Taking this implementation one step further, I wanted to use the Stable Marriage Algorithm iteratively, to join stable pairs to other stable pairs to forms groups (or sets). This post explains my implementation...","categories": ["edtech,","coding,","programming,","python,","computing,","computer-science"],
-        "tags": [],
-        "url": "/edtech,/coding,/programming,/python,/computing,/computer-science/StableGroups/",
+        "excerpt":"In my previous post, I explained my implementation of the Stable Marriage Algorithm to find stable pairs. Taking this implementation one step further, I wanted to use the Stable Marriage Algorithm iteratively, to join stable pairs to other stable pairs to forms groups (or sets). This post explains my implementation...","categories": ["data science"],
+        "tags": ["data science","stable marriage","algorithmic programming","python"],
+        "url": "/data%20science/StableGroups/",
         "teaser": null
       },{
         "title": "Build your own Blockchain Protocol",
-        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["Medium,","TowardsDataScience,","PeopleAnalytics,","Graphs,","Neo4j"],
-        "tags": [],
-        "url": "/medium,/towardsdatascience,/peopleanalytics,/graphs,/neo4j/build-your-own-blockchain-protocol/",
+        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["blockchain"],
+        "tags": ["python","distributed ledger","proof of work","flask"],
+        "url": "/blockchain/build-your-own-blockchain-protocol/",
         "teaser": null
       },{
         "title": "Build a Machine Learning Recommender",
-        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["Medium,","TowardsDataScience,","PeopleAnalytics,","Graphs,","Neo4j,","MachineLearning,","RecommenderSystems,","Recommender"],
-        "tags": [],
-        "url": "/medium,/towardsdatascience,/peopleanalytics,/graphs,/neo4j,/machinelearning,/recommendersystems,/recommender/build-a-machine-learning-recommender/",
+        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["recommender systems"],
+        "tags": ["python","collaborative filtering","content filtering","hybrid recommender"],
+        "url": "/recommender%20systems/build-a-machine-learning-recommender/",
         "teaser": null
       },{
         "title": "A New Era for People Analytics",
-        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.  ","categories": ["Medium,","TowardsDataScience,","PeopleAnalytics,","Graphs,","Neo4j"],
-        "tags": [],
-        "url": "/medium,/towardsdatascience,/peopleanalytics,/graphs,/neo4j/a-new-era-for-people-analytics/",
+        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.  ","categories": ["knowledge graphs"],
+        "tags": ["people analytics","graph","neo4j"],
+        "url": "/knowledge%20graphs/a-new-era-for-people-analytics/",
         "teaser": null
       },{
         "title": "Can your People Analytics do this?",
-        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["Medium,","TowardsDataScience,","People","Analytics"],
-        "tags": [],
-        "url": "/medium,/towardsdatascience,/people/analytics/can-your-people-analytics-do-this/",
+        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["knowledge graphs"],
+        "tags": ["people analytics","graph","neo4j"],
+        "url": "/knowledge%20graphs/can-your-people-analytics-do-this/",
         "teaser": null
       },{
         "title": "Top 10 architectural highlights for Digital Ocean Kubernetes",
-        "excerpt":"Recently I’ve been developing a solution architecture for a boostrapped startup in Digital Ocean’s Kubernetes. Developing an understanding of the context, discovering the domain and taking initial ideas through critical design thinking has been key to a foundational architecture that should serve this product well throughout its lifecycle. As envisioning...","categories": ["Kubernetes,","Digital","Ocean,","Solution","Architecture"],
-        "tags": [],
-        "url": "/kubernetes,/digital/ocean,/solution/architecture/kubernetes-digital-ocean/",
+        "excerpt":"Recently I’ve been developing a solution architecture for a boostrapped startup in Digital Ocean’s Kubernetes. Developing an understanding of the context, discovering the domain and taking initial ideas through critical design thinking has been key to a foundational architecture that should serve this product well throughout its lifecycle. As envisioning...","categories": ["kubernetes"],
+        "tags": ["architecture","kubernetes","open source","scaling","startups","scaleups"],
+        "url": "/kubernetes/kubernetes-digital-ocean/",
         "teaser": null
       },{
         "title": "Backup and Restore Neo4j in a Casual Cluster",
-        "excerpt":"If you’re managing a data engine inside a kubernetes cluster then implementing a backup and restore process can be challenging. A few months ago I developed a solution architecture deploying Neo4j into Kubernetes as a casual cluster. There’s a Medium post by Neo4j’s David Allen to explain what that configuration...","categories": ["Kubernetes,","Neo4j,","Solution","Architecture"],
-        "tags": [],
-        "url": "/kubernetes,/neo4j,/solution/architecture/neo4j-backup-restore/",
+        "excerpt":"If you’re managing a data engine inside a kubernetes cluster then implementing a backup and restore process can be challenging. A few months ago I developed a solution architecture deploying Neo4j into Kubernetes as a casual cluster. There’s a Medium post by Neo4j’s David Allen to explain what that configuration...","categories": ["kubernetes"],
+        "tags": ["causual cluster","neo4j","backup","restore","kubernetes","architecture"],
+        "url": "/kubernetes/neo4j-backup-restore/",
         "teaser": null
       },{
         "title": "Injecting Config as Environment Variables from Hasicorp's Consul & Vault",
-        "excerpt":"Continuing with the theme of Kubernetes, I have recently built out a solution to inject environment variables into containerised applications from Hasicorp Consult and Vault Key Value (KV) engine, which might be considered as a first step in realising Hashicorp’s Service Mesh. Installing both Consul and Vault via helm with...","categories": ["Kubernetes,","Neo4j,","Solution","Architecture,","Hasicorp,","Vault,","Consul"],
-        "tags": [],
-        "url": "/kubernetes,/neo4j,/solution/architecture,/hasicorp,/vault,/consul/vault-template/",
+        "excerpt":"Continuing with the theme of Kubernetes, I have recently built out a solution to inject environment variables into containerised applications from Hasicorp Consult and Vault Key Value (KV) engine, which might be considered as a first step in realising Hashicorp’s Service Mesh. Installing both Consul and Vault via helm with...","categories": ["kubernetes"],
+        "tags": ["architecture","hashicorp","consul","neo4j","containerisation","vault"],
+        "url": "/kubernetes/vault-template-2/",
         "teaser": null
       },{
         "title": "Gas System of the Future, Digital Twin",
-        "excerpt":"This article was published on Medium. Please click here to access the article.  ","categories": ["IBM","E&U"],
-        "tags": ["E&U","Energy","IBM"],
-        "url": "/ibm/e&u/gas-system-of-the-future/",
+        "excerpt":"This article was published on Medium. Please click here to access the article.  ","categories": ["digital twins"],
+        "tags": ["architecture","energy & utilities","digital twins","future energy system","energy vector integration","ibm","IBM App Connect Enterprise","IBM API Connect","Red Hat OpenShift","predictive modelling"],
+        "url": "/digital%20twins/gas-system-of-the-future/",
         "teaser": null
       },{
         "title": "Graph-Driven, LLM-Assisted Virtual Assistant Architecture",
-        "excerpt":"View the post here: https://jamesdhope.medium.com/graph-driven-llm-assisted-virtual-assistant-architecture-c1e4857a7040.  ","categories": ["GenAI","AI","LLMs"],
-        "tags": ["data science","GenAI","LLMs"],
-        "url": "/genai/ai/llms/graph-driven-llm-assistant-virtual-assistant/",
+        "excerpt":"View the post here: https://jamesdhope.medium.com/graph-driven-llm-assisted-virtual-assistant-architecture-c1e4857a7040.  ","categories": ["generativeAI"],
+        "tags": ["data science","generativeAI","llms","virtual assistants","graph","watsonx","ibm","architecture"],
+        "url": "/generativeai/graph-driven-llm-assistant-virtual-assistant/",
         "teaser": null
       }]
