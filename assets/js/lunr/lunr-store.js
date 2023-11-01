d420e97c5fae89ea8f46cdb055cb132d94979b42
James Hope <james.hope@ibm.com>
2023-11-01 10:12:07 +0000

    updated tags

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Deep Learning Recurrent Neural Net for Time Series Forecasting",
         "excerpt":"The Notebook described here supersedes my basic Recurrent Neural Net (RNN) for predicting multiple time series for the Wikipedia page forecasting competition. Instead, this RNN is constructed using the GRUCell rather than the BasicRNNCell which provides the model with persistence in the patterns observed over the series. The introductory paper...","categories": ["data science"],
-        "tags": ["data science","machine learning","neural networks","time series prediction","tensorflow","pandas","predictive modelling"],
+        "tags": ["data science","machine learning","neural networks","time series prediction","tensorflow","predictive modelling"],
         "url": "/data%20science/time-series-forecasting/",
         "teaser": null
       },{
@@ -24,9 +24,9 @@ var store = [{
         "teaser": null
       },{
         "title": "Build your own Blockchain Protocol",
-        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["blockchain"],
+        "excerpt":"This article was published in the journal Towards Data Science. Please click here to access the article.   ","categories": ["blockchain","system design"],
         "tags": ["python","distributed ledger","proof of work","flask"],
-        "url": "/blockchain/build-your-own-blockchain-protocol/",
+        "url": "/blockchain/system%20design/build-your-own-blockchain-protocol/",
         "teaser": null
       },{
         "title": "Build a Machine Learning Recommender",
@@ -48,32 +48,32 @@ var store = [{
         "teaser": null
       },{
         "title": "Top 10 architectural highlights for Digital Ocean Kubernetes",
-        "excerpt":"Recently I’ve been developing a solution architecture for a boostrapped startup in Digital Ocean’s Kubernetes. Developing an understanding of the context, discovering the domain and taking initial ideas through critical design thinking has been key to a foundational architecture that should serve this product well throughout its lifecycle. As envisioning...","categories": ["kubernetes"],
-        "tags": ["architecture","kubernetes","open source","scaling","startups","scaleups"],
-        "url": "/kubernetes/kubernetes-digital-ocean/",
+        "excerpt":"Recently I’ve been developing a solution architecture for a boostrapped startup in Digital Ocean’s Kubernetes. Developing an understanding of the context, discovering the domain and taking initial ideas through critical design thinking has been key to a foundational architecture that should serve this product well throughout its lifecycle. As envisioning...","categories": ["kubernetes","architecture"],
+        "tags": ["architecture","kubernetes","open source"],
+        "url": "/kubernetes/architecture/kubernetes-digital-ocean/",
         "teaser": null
       },{
         "title": "Backup and Restore Neo4j in a Casual Cluster",
-        "excerpt":"If you’re managing a data engine inside a kubernetes cluster then implementing a backup and restore process can be challenging. A few months ago I developed a solution architecture deploying Neo4j into Kubernetes as a casual cluster. There’s a Medium post by Neo4j’s David Allen to explain what that configuration...","categories": ["kubernetes"],
-        "tags": ["causual cluster","neo4j","backup","restore","kubernetes","architecture"],
-        "url": "/kubernetes/neo4j-backup-restore/",
+        "excerpt":"If you’re managing a data engine inside a kubernetes cluster then implementing a backup and restore process can be challenging. A few months ago I developed a solution architecture deploying Neo4j into Kubernetes as a casual cluster. There’s a Medium post by Neo4j’s David Allen to explain what that configuration...","categories": ["kubernetes","architecture"],
+        "tags": ["causual cluster","neo4j","backup & restore","kubernetes","architecture"],
+        "url": "/kubernetes/architecture/neo4j-backup-restore/",
         "teaser": null
       },{
         "title": "Injecting Config as Environment Variables from Hasicorp's Consul & Vault",
         "excerpt":"Continuing with the theme of Kubernetes, I have recently built out a solution to inject environment variables into containerised applications from Hasicorp Consult and Vault Key Value (KV) engine, which might be considered as a first step in realising Hashicorp’s Service Mesh. Installing both Consul and Vault via helm with...","categories": ["kubernetes"],
-        "tags": ["architecture","hashicorp","consul","neo4j","containerisation","vault"],
+        "tags": ["architecture","hashicorp consul","neo4j","containerisation","hashicorp vault"],
         "url": "/kubernetes/vault-template-2/",
         "teaser": null
       },{
         "title": "Gas System of the Future, Digital Twin",
-        "excerpt":"This article was published on Medium. Please click here to access the article.  ","categories": ["digital twins"],
+        "excerpt":"This article was published on Medium. Please click here to access the article.  ","categories": ["digital twins","architecture"],
         "tags": ["architecture","energy & utilities","digital twins","future energy system","energy vector integration","ibm","IBM App Connect Enterprise","IBM API Connect","Red Hat OpenShift","predictive modelling"],
-        "url": "/digital%20twins/gas-system-of-the-future/",
+        "url": "/digital%20twins/architecture/gas-system-of-the-future/",
         "teaser": null
       },{
         "title": "Graph-Driven, LLM-Assisted Virtual Assistant Architecture",
-        "excerpt":"View the post here: https://jamesdhope.medium.com/graph-driven-llm-assisted-virtual-assistant-architecture-c1e4857a7040.  ","categories": ["generativeAI"],
+        "excerpt":"View the post here: https://jamesdhope.medium.com/graph-driven-llm-assisted-virtual-assistant-architecture-c1e4857a7040.  ","categories": ["generativeAI","architecture"],
         "tags": ["data science","generativeAI","llms","virtual assistants","graph","watsonx","ibm","architecture"],
-        "url": "/generativeai/graph-driven-llm-assistant-virtual-assistant/",
+        "url": "/generativeai/architecture/graph-driven-llm-assistant-virtual-assistant/",
         "teaser": null
       }]
